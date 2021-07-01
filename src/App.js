e9165b8f1e5a3ea822cb21a4920ba5dab9aa0026
Wojciech Jarosz <jaroszw@gmail.com>
2021-07-01 12:58:17 +0200

    animations

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
   };
 
   showModal = () => {
-    console.log("clicked");
     this.setState({ modalIsOpen: true });
   };
 
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,13 +3,7 @@ import React from "react";
 import "./Modal.css";
 
 const modal = (props) => {
-  const cssClasses = [
-    "Backdrop",
-    props.show ? "BackdropOpen" : "BackdropClosed",
-  ];
-
-  console.log(props);
-
+  const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClosed"];
   return (
     <div className={cssClasses.join(" ")}>
       <h1>A Modal</h1>
